ca8aafe4efc3e9daf1530d345835aa19bbed2ebc
dansab <dan.sabadas@gmail.com>
2015-11-22 12:24:02 +0200

    spec comm

--- test/hello_spec.js
+++ test/hello_spec.js
@@ -1,6 +1,6 @@
 ﻿var sayHello = require('../src/hello');
 describe("Hello", function () {
   it("says hello", function () {
-    expect(sayHello('Jane')).toBe("Hello, Jane!");
+    expect(sayHello('Jan')).toBe("Hello, Jan!");
   });
 });
