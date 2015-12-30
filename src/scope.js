3c5ca8a768398c09c49bf02c1a9b0ca7f9caf256
dansab <dan.sabadas@gmail.com>
2015-12-30 16:27:58 +0200

    arrived at chapter 2 Scope Inheritance pg 67

--- src/scope.js
+++ src/scope.js
@@ -171,17 +171,37 @@ Scope.prototype.$$postDigest = function (fn) {
   this.$$postDigestQueue.push(fn);
 };
 
-Scope.prototype.$watchGroup = function (watchFns, listenerFn) {
+Scope.prototype.$watchGroup = function(watchFns, listenerFn) {
   var self = this;
-  var newValues = new Array(watchFns.length);
   var oldValues = new Array(watchFns.length);
+  var newValues = new Array(watchFns.length);
   var changeReactionScheduled = false;
+  var firstRun = true;
+  if (watchFns.length === 0) {
+    var shouldCall = true;
+    self.$evalAsync(function() {
+      if (shouldCall) {
+        listenerFn(newValues, newValues, self);
+      }
+    });
+
+    return function () {
+      shouldCall = false;
+    };
+  }
+
   function watchGroupListener() {
-    listenerFn(newValues, oldValues, self);
+    if (firstRun) {
+      firstRun = false;
+      listenerFn(newValues, newValues, self);
+    } else {
+      listenerFn(newValues, oldValues, self);
+    }
     changeReactionScheduled = false;
   }
-  _.forEach(watchFns, function (watchFn, i) {
-    self.$watch(watchFn, function (newValue, oldValue) {
+
+  var destroyFunctions = _.map(watchFns, function(watchFn, i) {
+    return self.$watch(watchFn, function(newValue, oldValue) {
       newValues[i] = newValue;
       oldValues[i] = oldValue;
       if (!changeReactionScheduled) {
@@ -190,6 +210,12 @@ Scope.prototype.$watchGroup = function (watchFns, listenerFn) {
       }
     });
   });
+
+  return function () {
+    _.forEach(destroyFunctions, function (destroyFunction) {
+      destroyFunction();
+    });
+  };
 };
 
 module.exports = Scope;
