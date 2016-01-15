5706ef0428f93573827f3a60b1ced6a3bfa53fc9
fortechdotnet <dan.sabadas@gmail.com>
2016-01-15 11:44:43 +0200

    arrived at Chapter 4 Scope Events

--- src/scope.js
+++ src/scope.js
@@ -9,26 +9,10 @@ function isArrayLike(obj) {
     return false;
   }
   var length = obj.length;
-  var isNumber = _.isNumber(length);
-  if (!isNumber) {
-    return false;
-  }
-  if (length === 0) {
-    return true;
-  } else if (length > 0) {
-    return (length - 1) in obj;
-  } else {
-    return false;
-  }
-}
 
-//function isArrayLike(obj) {
-//  if (_.isNull(obj) || _.isUndefined(obj)) {
-//    return false;
-//  }
-//  var length = obj.length;
-//  return _.isNumber(length);
-//}
+  return length === 0 ||
+    (_.isNumber(length) && length > 0 && (length - 1) in obj);
+}
 
 function Scope() {
   this.$$watchers = [];
@@ -300,8 +284,13 @@ Scope.prototype.$watchCollection = function (watchFn, listenerFn) {
   var self = this;
   var newValue;
   var oldValue;
+  var oldLength;
+  var veryOldValue;
+  var trackVeryOldValue = (listenerFn.length > 1);
   var changeCount = 0;
+  var firstRun = true;
   var internalWatchFn = function (scope) {
+    var newLength;
     newValue = watchFn(scope);
     if (_.isObject(newValue)) {
       if (isArrayLike(newValue)) {
@@ -321,6 +310,35 @@ Scope.prototype.$watchCollection = function (watchFn, listenerFn) {
           }
         });
       } else {
+        if (!_.isObject(oldValue) || isArrayLike(oldValue)) {
+          changeCount++;
+          oldValue = {};
+          oldLength = 0;
+        }
+        newLength = 0;
+        _.forOwn(newValue, function (newVal, key) {
+          newLength++;
+          if (oldValue.hasOwnProperty(key)) {
+            var bothNaN = _.isNaN(newVal) && _.isNaN(oldValue[key]);
+            if (!bothNaN && oldValue[key] !== newVal) {
+              changeCount++;
+              oldValue[key] = newVal;
+            }
+          } else {
+            changeCount++;
+            oldLength++;
+            oldValue[key] = newVal;
+          }
+        });
+        if (oldLength > newLength) {
+          changeCount++;
+          _.forOwn(oldValue, function (oldVal, key) {
+            if (!newValue.hasOwnProperty(key)) {
+              oldLength--;
+              delete oldValue[key];
+            }
+          });
+        }
       }
     } else {
       if (!self.$$areEqual(newValue, oldValue, false)) {
@@ -330,8 +348,17 @@ Scope.prototype.$watchCollection = function (watchFn, listenerFn) {
     }
     return changeCount;
   };
-  var internalListenerFn = function() {
-    listenerFn(newValue, oldValue, self);
+  var internalListenerFn = function () {
+    if (firstRun) {
+      listenerFn(newValue, newValue, self);
+      firstRun = false;
+    } else {
+      listenerFn(newValue, veryOldValue, self);
+    }
+
+    if (trackVeryOldValue) {
+      veryOldValue = _.clone(newValue);
+    }
   };
   return this.$watch(internalWatchFn, internalListenerFn);
 };
