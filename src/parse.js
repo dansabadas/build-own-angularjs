ad66126258db615daa4559c83309a41778fc87c9
Dan Sabadis <dan_sabadas@hotmail.com>
2016-03-01 09:47:50 +0200

    arrived at logical operators

--- src/parse.js
+++ src/parse.js
@@ -13,7 +13,16 @@ var OPERATORS = {
   '-': true,
   '*': true,
   '/': true,
-  '%': true
+  '%': true,
+  '=': true,
+  '==': true,
+  '!=': true,
+  '===': true,
+  '!==': true,
+  '<': true,
+  '>': true,
+  '<=': true,
+  '>=': true
 };
 
 function ifDefined(value, defaultValue) {
@@ -84,7 +93,7 @@ Lexer.prototype.lex = function (text) {
       this.readNumber();
     } else if (this.is('\'"')) {
       this.readString(this.ch);
-    } else if (this.is('[],{}:.()=')) {
+    } else if (this.is('[],{}:.()')) {
       this.tokens.push({
         text: this.ch
       });
@@ -94,10 +103,16 @@ Lexer.prototype.lex = function (text) {
     } else if (this.isWhitespace(this.ch)) {
       this.index++;
     } else {
-      var op = OPERATORS[this.ch];
-      if (op) {
-        this.tokens.push({ text: this.ch });
-        this.index++;
+      var ch = this.ch;
+      var ch2 = this.ch + this.peek();
+      var ch3 = this.ch + this.peek() + this.peek(2);
+      var op = OPERATORS[ch];
+      var op2 = OPERATORS[ch2];
+      var op3 = OPERATORS[ch3];
+      if (op || op2 || op3) {
+        var token = op3 ? ch3 : (op2 ? ch2 : ch);
+        this.tokens.push({ text: token });
+        this.index += token.length;
       } else {
         throw 'Unexpected next character: ' + this.ch;
       }
@@ -206,9 +221,10 @@ Lexer.prototype.readString = function (quote) {
   }
   throw 'Unmatched quote';
 };
-Lexer.prototype.peek = function () {
-  return this.index < this.text.length - 1 ?
-  this.text.charAt(this.index + 1) :
+Lexer.prototype.peek = function (n) {
+  n = n || 1;
+  return this.index + n < this.text.length ?
+  this.text.charAt(this.index + n) :
   false;
 };
 
@@ -237,6 +253,32 @@ AST.prototype.program = function () {
 };
 AST.prototype.identifier = function () {
   return { type: AST.Identifier, name: this.consume().text };
+};
+AST.prototype.equality = function() {
+  var left = this.relational();
+  var token;
+  while ((token = this.expect('==', '!=', '===', '!=='))) {
+    left = {
+      type: AST.BinaryExpression,
+      left: left,
+      operator: token.text,
+      right: this.relational()
+    };
+  }
+  return left;
+};
+AST.prototype.relational = function() {
+  var left = this.additive();
+  var token;
+  while ((token = this.expect('<', '>', '<=', '>='))) {
+    left = {
+      type: AST.BinaryExpression,
+      left: left,
+      operator: token.text,
+      right: this.additive()
+    };
+  }
+  return left;
 };
 AST.prototype.additive = function () {
   var left = this.multiplicative();
@@ -277,9 +319,9 @@ AST.prototype.unary = function () {
   }
 };
 AST.prototype.assignment = function () {
-  var left = this.additive();
+  var left = this.equality();
   if (this.expect('=')) {
-    var right = this.additive();
+    var right = this.equality();
     return { type: AST.AssignmentExpression, left: left, right: right };
   }
   return left;
