2d5c96664a55c3879c5ed9be69c184f350508762
Dan Sabadis <dan_sabadas@hotmail.com>
2016-03-02 15:17:56 +0200

    arrived at The Filter Filter

--- test/filter_spec.js
+++ test/filter_spec.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var _ = require('lodash');
 var register = require('../src/filter').register;
 var filter = require('../src/filter').filter;
 var parse = require('../src/parse');
@@ -35,5 +36,37 @@ describe("filter", function () {
     });
     var fn = parse('aString | upcase');
     expect(fn({ aString: 'Hello' })).toEqual('HELLO');
+  });
+  it('can parse filter chain expressions', function () {
+    register('upcase', function () {
+      return function (s) {
+        return s.toUpperCase();
+      };
+    });
+    register('exclamate', function () {
+      return function (s) {
+        return s + '!';
+      };
+    });
+    var fn = parse('"hello" | upcase | exclamate');
+    expect(fn()).toEqual('HELLO!');
+  });
+  it('can pass an additional argument to filters', function () {
+    register('repeat', function () {
+      return function (s, times) {
+        return _.repeat(s, times);
+      };
+    });
+    var fn = parse('"hello" | repeat:3');
+    expect(fn()).toEqual('hellohellohello');
+  });
+  it('can pass several additional arguments to filters', function () {
+    register('surround', function () {
+      return function (s, left, right) {
+        return left + s + right;
+      };
+    });
+    var fn = parse('"hello" | surround:"*":"!"');
+    expect(fn()).toEqual('*hello!');
   });
 }); 
