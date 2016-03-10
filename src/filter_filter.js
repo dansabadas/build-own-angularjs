b753283b6c4c5c8262511ed2c55d597e9641a155
Dan Sabadis <dan.sabadas@gmail.com>
2016-03-10 11:14:57 +0200

    arrived at Chapter 8 Filters - Negated Filtering With Strings pg 318

---
+++ src/filter_filter.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var _ = require('lodash');
+
+function deepCompare(actual, expected, comparator) {
+  if (_.isObject(actual)) {
+    return _.some(actual, function (value) {
+      return deepCompare(value, expected, comparator);
+    });
+  } else {
+    return comparator(actual, expected);
+  }
+}
+
+function createPredicateFn(expression) {
+  function comparator(actual, expected) {
+    if (_.isUndefined(actual)) {
+      return false;
+    }
+    if (_.isNull(actual) || _.isNull(expected)) {
+      return actual === expected;
+    }
+    actual = ('' + actual).toLowerCase();
+    expected = ('' + expected).toLowerCase();
+    return actual.indexOf(expected) !== -1;
+  }
+  return function predicateFn(item) {
+    return deepCompare(item, expression, comparator);
+  };
+}
+
+function filterFilter() {
+  return function (array, filterExpr) {
+    var predicateFn;
+    if (_.isFunction(filterExpr)) {
+      predicateFn = filterExpr;
+    } else if (_.isString(filterExpr) ||
+               _.isNumber(filterExpr) ||
+               _.isBoolean(filterExpr) ||
+               _.isNull(filterExpr)) {
+      predicateFn = createPredicateFn(filterExpr);
+    } else {
+      return array;
+    }
+    return _.filter(array, predicateFn);
+  };
+}
+
+module.exports = filterFilter;
