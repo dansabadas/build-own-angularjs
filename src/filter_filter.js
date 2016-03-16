ea5e26a453e118ad5b890074071c78a25d3345d2
Dan Sabadis <dan.sabadas@gmail.com>
2016-03-16 09:46:50 +0200

    arrived at p328

--- src/filter_filter.js
+++ src/filter_filter.js
@@ -2,11 +2,35 @@
 
 var _ = require('lodash');
 
-function deepCompare(actual, expected, comparator) {
-  if (_.isObject(actual)) {
-    return _.some(actual, function (value) {
-      return deepCompare(value, expected, comparator);
+function deepCompare(actual, expected, comparator, matchAnyProperty) {
+  if (_.isString(expected) && _.startsWith(expected, '!')) {
+    return !deepCompare(actual, expected.substring(1), comparator, matchAnyProperty);
+  }
+  if (_.isArray(actual)) {
+    return _.any(actual, function (actualItem) {
+      return deepCompare(actualItem, expected, comparator, matchAnyProperty);
     });
+  }
+  if (_.isObject(actual)) {
+    if (_.isObject(expected)) {
+      return _.every(
+        _.toPlainObject(expected),
+        function (expectedVal, expectedKey) {
+          if (_.isUndefined(expectedVal)) {
+            return true;
+          }
+          var isWildcard = (expectedKey === '$');
+          var actualVal = isWildcard ? actual : actual[expectedKey];
+          return deepCompare(actualVal, expectedVal, comparator, isWildcard);
+        }
+      );
+    } else if (matchAnyProperty) {
+      return _.some(actual, function (value, key) {
+        return deepCompare(value, expected, comparator, matchAnyProperty);
+      });
+    } else {
+      return comparator(actual, expected);
+    }
   } else {
     return comparator(actual, expected);
   }
@@ -25,7 +49,7 @@ function createPredicateFn(expression) {
     return actual.indexOf(expected) !== -1;
   }
   return function predicateFn(item) {
-    return deepCompare(item, expression, comparator);
+    return deepCompare(item, expression, comparator, true);
   };
 }
 
@@ -37,7 +61,8 @@ function filterFilter() {
     } else if (_.isString(filterExpr) ||
                _.isNumber(filterExpr) ||
                _.isBoolean(filterExpr) ||
-               _.isNull(filterExpr)) {
+               _.isNull(filterExpr) ||
+               _.isObject(filterExpr)) {
       predicateFn = createPredicateFn(filterExpr);
     } else {
       return array;
